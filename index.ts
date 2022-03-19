b50731bb1e9b09ddf85bf9ca830c51b8c4a9a732
scott <scottliyq@gmail.com>
2022-03-19 22:40:13 +0800

    start to modify

--- index.ts
+++ index.ts
@@ -10,6 +10,8 @@ import { setBech32NetworkPrefix } from 'cosmos-client/esm/types/address/config'
 import { HdPath, stringToPath } from "@cosmjs/crypto";
 import { AccAddress } from 'cosmos-client/cjs/types';
 import { cwd } from "process";
+import { Decimal } from "@cosmjs/math";
+import { Delegation } from "cosmos-client/cjs/openapi/api";
 
 const { SecretClient } = require("@azure/keyvault-secrets");
 const { DefaultAzureCredential } = require("@azure/identity");
@@ -26,12 +28,55 @@ interface IChainConfiguration{
   customDerivationPath? : string
 }
 
+interface IAccountConfiguration{
+  accountName : string,
+  mnemonic : string,
+  chainNames : string,
+  claimRewards : boolean,
+  stakeAvailableBalance : boolean,
+  leaveMinimumBalance : Decimal
+}
+
+//保存各个账户的返回信息
+interface IAccountInfo{
+  accountName : string,
+  accountInfo : IAccountChainInfo[],
+  accountStakeInfo : IStakeChainInfo[],
+
+}
+
+interface IAccountChainInfo{
+  chainName : string,
+  chainAddress: string,
+  tokens : ITokenInfo[],
+}
+
+interface IStakeChainInfo{
+  chainName : string,
+  chainAddress: string,
+  token : ITokenInfo,
+}
+
+interface ITokenInfo{
+  denom: string,
+  amount: Number
+}
+
+const MapAccountInfo : Record<string, IAccountInfo> = {};
+
+
 const chainConfigs = require('./chainConfigs.json') as IChainConfiguration[];
+
+const accountConfigs = require('./accountConfigs.json') as IAccountConfiguration[];
+
+const tokenMap = require('./tokenMap.json') as Record<string, string>;
+
+const MIN_REWARD_AMOUNT = 200000
 
 interface IAppParameters{
   _ : any[],
-  chainNames : string,
-  mnemonic? : string,
+  // chainNames : string,
+  // mnemonic? : string,
   keyVaultName? : string,
   keyVaultSecretName? : string,
   claimRewards : boolean,
@@ -48,27 +93,67 @@ interface IClientInfos {
 async function main(){
   try{
     const parameters = await GetAndVerifyParameters();
-    const chainNames = parameters.chainNames.split(',');
-    for( let chainName of chainNames ){
-      const filteredConfigs = chainConfigs.filter( (c : IChainConfiguration) => c.chainName == chainName );
-      if(filteredConfigs === undefined || filteredConfigs.length == 0)
-      {
-        console.log(`\nCould not find configuration for chain by given name '${chainName}'! Continuing...`);
-        continue;
-      }
-      
-      const config = filteredConfigs[0];
-  
-      const clients = await InitClients( config, parameters );
-      await ClaimRewards(config, clients, parameters);
-      await StakeBalance(config, clients, parameters);
+    //define chain names
+    // const chainNames = parameters.chainNames.split(',');
+    for( let accountConfig of accountConfigs ){
+      let accountName = accountConfig.accountName;
+
+      let accountChain: IAccountInfo = {
+        accountName: accountName,
+        accountInfo: [],
+        accountStakeInfo:[]
+      };
+
+      MapAccountInfo[accountName] = accountChain;
     }
+    for( let accountConfig of accountConfigs ){
+      // console.log(accountConfig.accountName);
+      const chainNames = accountConfig.chainNames.split(',');
+      for( let chainName of chainNames ){
+        const filteredConfigs = chainConfigs.filter( (c : IChainConfiguration) => c.chainName == chainName );
+        if(filteredConfigs === undefined || filteredConfigs.length == 0)
+        {
+          console.log(`\nCould not find configuration for chain by given name '${chainName}'! Continuing...`);
+          continue;
+        }
+        
+        const config = filteredConfigs[0];
+        //define cosmos client
+        const clients = await InitClients( config, parameters , accountConfig);
+        let accountName = accountConfig.accountName;
+        
+        // todo 
+        if (parameters.claimRewards) {
+          await ClaimRewards(config, clients, parameters, accountName);
+
+        } else {
+          await GetAllBalance(config, clients, parameters, accountConfig, accountName);
+          // 查询多链staking 的balance
+          await GetStakingBalance(config, clients, parameters, accountConfig, accountName);
+
+        }
+
+
+        if (parameters.stakeAvailableBalance) {
+          await StakeBalance(config, clients, parameters, accountName);
+        }
+        // await StakeBalance(config, clients, parameters);
+        
+      }
+    
+    }
+    if (!parameters.claimRewards) {
+      //todo 输出balance的查询结果
+      console.log(JSON.stringify(MapAccountInfo, null, 4));
+    }
+    
   } catch( error : any ) {
     if(error.message !== undefined)
       console.error(error.message);
     else
       console.error( error );
   }
+
 }
 
 function setFromFromEnv( args: any, memberName: string, varName: string ){
@@ -80,13 +165,13 @@ function setFromFromEnv( args: any, memberName: string, varName: string ){
 
 async function GetAndVerifyParameters() : Promise<IAppParameters>{
   var args = minimist(process.argv.slice(2), {
-    string: [ 'chainNames', 'mnemonic', 'keyVaultName', 'keyVaultSecretName', 'leaveMinimumBalance' ],
+    string: [ 'keyVaultName', 'keyVaultSecretName', 'leaveMinimumBalance' ],
     boolean: [ 'claimRewards', 'stakeAvailableBalance' ],
     //alias: { h: 'help', v: 'version' },
     default: { 
       claimRewards: false, 
       stakeAvailableBalance: false, 
-      leaveMinimumBalance: 500000 
+      leaveMinimumBalance: 50000
     },
     '--': false,
     stopEarly: true, /* populate _ with first non-option */
@@ -95,8 +180,8 @@ async function GetAndVerifyParameters() : Promise<IAppParameters>{
     }
   });
 
-  setFromFromEnv( args, "chainNames", "STAKEBOT_CHAIN_NAMES");
-  setFromFromEnv( args, "mnemonic", "STAKEBOT_MNEMONIC");
+  // setFromFromEnv( args, "chainNames", "STAKEBOT_CHAIN_NAMES");
+  // setFromFromEnv( args, "mnemonic", "STAKEBOT_MNEMONIC");
   setFromFromEnv( args, "keyVaultName", "STAKEBOT_KEY_VAULT_NAME");
   setFromFromEnv( args, "keyVaultSecretName", "STAKEBOT_KEY_VAULT_SECRET_NAME");
   setFromFromEnv( args, "claimRewards", "STAKEBOT_CLAIM_REWARDS" );
@@ -105,32 +190,33 @@ async function GetAndVerifyParameters() : Promise<IAppParameters>{
 
   const params = args as IAppParameters;
 
-  if(params.chainNames === undefined ){
-    throw new Error(`Missing application argument 'chainNames' (comma-separated string or names) or environment variable STAKEBOT_CHAIN_NAMES`);
-  }
+  // if(params.chainNames === undefined ){
+  //   throw new Error(`Missing application argument 'chainNames' (comma-separated string or names) or environment variable STAKEBOT_CHAIN_NAMES`);
+  // }
 
-  if(params.mnemonic === undefined && ( params.keyVaultName === undefined || params.keyVaultSecretName === undefined ) ){
-    throw new Error(`Application requires mnemonic for your wallet. Either\n1. Supply it directly using the 'mnemonic' CLI parameter or environment variable STAKEBOT_MNEMONIC\n2. Fetch it from Azure Key Vault by defining CLI parameters 'keyVaultName' + 'keyVaultSecretName' or environment variables STAKEBOT_KEYVAULT_NAME + STAKEBOT_KEY_VAULT_SECRET_NAME in conjunction with environment variables AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET.`);
-  }
+  // if(params.mnemonic === undefined && ( params.keyVaultName === undefined || params.keyVaultSecretName === undefined ) ){
+  //   throw new Error(`Application requires mnemonic for your wallet. Either\n1. Supply it directly using the 'mnemonic' CLI parameter or environment variable STAKEBOT_MNEMONIC\n2. Fetch it from Azure Key Vault by defining CLI parameters 'keyVaultName' + 'keyVaultSecretName' or environment variables STAKEBOT_KEYVAULT_NAME + STAKEBOT_KEY_VAULT_SECRET_NAME in conjunction with environment variables AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET.`);
+  // }
 
-  if( params.keyVaultName !== undefined && params.keyVaultSecretName !== undefined){
-    console.log("Loading mnemonic from Azure Key Vault");
-    const credential = new DefaultAzureCredential();
+  // if( params.keyVaultName !== undefined && params.keyVaultSecretName !== undefined){
+  //   console.log("Loading mnemonic from Azure Key Vault");
+  //   const credential = new DefaultAzureCredential();
 
-    const url = `https://${params.keyVaultName}.vault.azure.net`;
-    const client = new SecretClient(url, credential);
+  //   const url = `https://${params.keyVaultName}.vault.azure.net`;
+  //   const client = new SecretClient(url, credential);
 
-    const response = await client.getSecret(params.keyVaultSecretName);
-    params.mnemonic = response.value;
-  }
+  //   const response = await client.getSecret(params.keyVaultSecretName);
+  //   params.mnemonic = response.value;
+  // }
 
   return params;
 }
 
-async function InitClients( config : IChainConfiguration, params : IAppParameters ) : Promise<IClientInfos> {
-  const wallet = await DirectSecp256k1HdWallet.fromMnemonic(params.mnemonic, { prefix: config.walletPrefix, hdPaths: config.customDerivationPath ? [stringToPath(config.customDerivationPath)] : undefined });
+//todo use accountFig instead
+async function InitClients( config : IChainConfiguration, params : IAppParameters ,accountConfig :IAccountConfiguration) : Promise<IClientInfos> {
+  const wallet = await DirectSecp256k1HdWallet.fromMnemonic(accountConfig.mnemonic, { prefix: config.walletPrefix, hdPaths: config.customDerivationPath ? [stringToPath(config.customDerivationPath)] : undefined });
   const [account] = await wallet.getAccounts();
-console.log(account.address);
+  // console.log(account.address);
 
   setBech32NetworkPrefix( config.walletPrefix );
 
@@ -141,26 +227,27 @@ console.log(account.address);
   const restClient = new cosmosclient.CosmosSDK(config.restEndpoint, config.networkName );
   
   const privKey = new proto.cosmos.crypto.secp256k1.PrivKey({
-      key: await cosmosclient.generatePrivKeyFromMnemonic(params.mnemonic),
+      key: await cosmosclient.generatePrivKeyFromMnemonic(accountConfig.mnemonic),
   });
   const pubKey = privKey.pubKey();
   const address = cosmosclient.AccAddress.fromPublicKey(pubKey);
 
-  console.log( `\nUsing wallet address ${address.toString()}`)
+  // console.log( `\nUsing wallet address ${address.toString()}`)
 
   return {rest:restClient, rpc: rpcClient, restAddress: address };
 }
 
-async function ClaimRewards( config : IChainConfiguration, clients : IClientInfos, params: IAppParameters ) {
+async function ClaimRewards( config : IChainConfiguration, clients : IClientInfos, params: IAppParameters, accoutnName: string) {
   let totalRewards = await rest.distribution
       .delegationTotalRewards(clients.rest, clients.restAddress )
       .then( res => res.data );
-  
+  console.log(`${accoutnName}: ${clients.restAddress}: start to claim >>>>>>>>>>>>>>`)
+
   if(totalRewards?.rewards?.length !== undefined && 
     totalRewards.rewards.length > 0 && 
     totalRewards?.total?.length !== undefined &&
     totalRewards.total.length > 0 ) {
-      console.log(`Total rewards awaiting claim: ${totalRewards?.total[0].amount} ${totalRewards.total[0].denom}`);
+      // console.log(`${accoutnName}: Total rewards awaiting claim: ${totalRewards?.total[0].amount} ${totalRewards.total[0].denom}`);
 
     if(params.claimRewards) {
         const fee = GenerateFee(config);
@@ -170,29 +257,35 @@ async function ClaimRewards( config : IChainConfiguration, clients : IClientInfo
           if(delegationRewards?.reward === undefined){
             continue;
           }
-
+          // console.log(delegationRewards);
           let validatorAddress = delegationRewards?.validator_address;
-          let rewardAmount = delegationRewards?.reward[0]?.amount;
-          let rewardDenom = delegationRewards?.reward[0]?.denom;
+
+          for( let reward of delegationRewards?.reward) {
+
+            // let rewardAmount = delegationRewards?.reward[0]?.amount;
+            // let rewardDenom = delegationRewards?.reward[0]?.denom;
+            let rewardAmount = reward.amount;
+            let rewardDenom = reward.denom;
+            if(validatorAddress === undefined || 
+              rewardAmount === undefined || 
+              rewardDenom === undefined) {
+              continue;
+            }
+  
+            console.log(`Rewards can be claimed: ${convertAmount(rewardAmount)} ${convertToken(rewardDenom)}`);
+  
+            if( parseInt(rewardAmount) > MIN_REWARD_AMOUNT) {
+                const withdrawResult = await clients.rpc.withdrawRewards( clients.restAddress.toString(), validatorAddress, fee );
+                // console.log("Withdraw result:\n" + JSON.stringify(withdrawResult));
+                var obj = JSON.parse((withdrawResult.rawLog as string).slice(1,-1));
+                const claimedAmount = obj.events.filter( (e : any) => e.type == "withdraw_rewards")[0].attributes.filter( (a : any) => a.key == "amount" )[0].value
+                console.log( "========== Claimed " + claimedAmount +  "==========");
+            }
+            else{
+                console.log("Not time to claim yet...");
+            }
+          }
           
-          if(validatorAddress === undefined || 
-            rewardAmount === undefined || 
-            rewardDenom === undefined) {
-            continue;
-          }
-
-          console.log(`Rewards from\nValidator: ${validatorAddress}\nValue: ${rewardAmount} ${rewardDenom}`);
-
-          if( parseInt(rewardAmount) > 1000000) {
-              const withdrawResult = await clients.rpc.withdrawRewards( clients.restAddress.toString(), validatorAddress, fee );
-              // console.log("Withdraw result:\n" + JSON.stringify(withdrawResult));
-              var obj = JSON.parse((withdrawResult.rawLog as string).slice(1,-1));
-              const claimedAmount = obj.events.filter( (e : any) => e.type == "withdraw_rewards")[0].attributes.filter( (a : any) => a.key == "amount" )[0].value
-              console.log( "Claimed " + claimedAmount );
-          }
-          else{
-              console.log("Not time to claim yet...");
-          }
         }
     }
   }
@@ -202,9 +295,10 @@ async function ClaimRewards( config : IChainConfiguration, clients : IClientInfo
   }
 }
 
-async function StakeBalance( config : IChainConfiguration, clients : IClientInfos, params: IAppParameters ) {
+async function StakeBalance( config : IChainConfiguration, clients : IClientInfos, params: IAppParameters, accoutnName: string) {
   //const foo = await clients.rpc.getAllBalances( clients.restAddress.toString());
   //console.log(JSON.stringify(foo));
+  console.log(`${accoutnName}: start to stake >>>>>>>>>>>>>>`)
   if( !params.stakeAvailableBalance ){
     console.log(`Not staking, as stakeAvailableBalance is set to ${params.stakeAvailableBalance}`)
     return;
@@ -215,10 +309,17 @@ async function StakeBalance( config : IChainConfiguration, clients : IClientInfo
     .then( res => res.data.delegation_responses );
   const orderedDelegations = delegations.sort( (a : any, b : any) => parseInt(a?.balance?.amount ?? "0") - parseInt(b?.balance?.amount ?? "0") );
   const validatorWithLeastStaked = orderedDelegations[0];
+  //todo 查询账户余额，只需要地址，不需要秘钥
   const currentAccountBalance = await clients.rpc.getBalance( clients.restAddress.toString(), config.tokenDenom);
   const currentAccountBalanceValue = parseInt(currentAccountBalance?.amount ?? "0");
+  // console.log( `leaveminimumbalance : ${params.leaveMinimumBalance} `)
+
+  const temp = params.leaveMinimumBalance
+  console.log( `leaveMinimumBalance : ${temp} `)
+
   if( currentAccountBalanceValue > params.leaveMinimumBalance * 2 )
   {
+      //todo 显示当前账户余额
       console.log(`Balance high enough to stake: ${currentAccountBalanceValue / 1000000} / ${params.leaveMinimumBalance * 2 / 1000000}`);
       const balanceToStake = currentAccountBalanceValue - params.leaveMinimumBalance;
       const delegateResult = await clients.rpc.delegateTokens(clients.restAddress.toString(), validatorWithLeastStaked.delegation?.validator_address, coin( balanceToStake.toString(), config.tokenDenom ), GenerateFee(config) );
@@ -228,6 +329,92 @@ async function StakeBalance( config : IChainConfiguration, clients : IClientInfo
   else{
     console.log( `Account balance ${currentAccountBalanceValue / 1000000} is too low. Won't stake.`)
   }
+}
+
+//显示所有链上的账户余额
+async function DisplayBalance( config : IChainConfiguration, clients : IClientInfos, params: IAppParameters, accountConfig :IAccountConfiguration) {
+
+  //todo 查询账户余额，只需要地址，不需要秘钥
+  const currentAccountBalance = await clients.rpc.getBalance( clients.restAddress.toString(), config.tokenDenom);
+  const currentAccountBalanceValue = parseInt(currentAccountBalance?.amount ?? "0");
+  console.log( `leaveminimumbalance : ${params.leaveMinimumBalance} `)
+  
+  // const balances = await clients.rpc.getAllBalances(clients.restAddress.toString());
+
+  console.log(`${clients.restAddress.toString()} Balance : ${currentAccountBalanceValue / 1000000}`);
+
+}
+
+//todo
+async function GetAllBalance( config : IChainConfiguration, clients : IClientInfos, params: IAppParameters, accountConfig :IAccountConfiguration, accountName: string)  {
+  // let accountChain1: IAccountChainInfo = {
+  //   chainName: 'cosmos',
+  //   chainAddress: "",
+  //   tokens:[]
+  // };
+  // let aryAccountChains: IAccountChainInfo[] = [];
+  //todo 查询账户余额，只需要地址，不需要秘钥
+  // const currentAccountBalance = await clients.rpc.getBalance( clients.restAddress.toString(), config.tokenDenom);
+  // const currentAccountBalanceValue = parseInt(currentAccountBalance?.amount ?? "0");
+  // console.log( `leaveminimumbalance : ${params.leaveMinimumBalance} `)
+  
+  const balances = await clients.rpc.getAllBalances(clients.restAddress.toString());
+  if (balances.length > 0) {
+    let accountChain: IAccountChainInfo = {
+      chainName: config.chainName,
+      chainAddress: clients.restAddress.toString(),
+      tokens:[]
+    };
+    for( let coin of balances ){
+      let tokenInfo: ITokenInfo = {
+        denom: convertToken(coin.denom),
+        amount: convertAmount(coin.amount)
+      };
+      accountChain.tokens.push(tokenInfo);
+    }
+    MapAccountInfo[accountName].accountInfo.push(accountChain)
+  }
+
+
+  // console.log(clients.restAddress.toString())
+  // console.log(1)
+  // console.log(balances);
+
+  // return aryAccountChains
+
+}
+
+//todo 
+async function GetStakingBalance( config : IChainConfiguration, clients : IClientInfos, params: IAppParameters, accountConfig :IAccountConfiguration, accountName: string) {
+  let stakeChain: IStakeChainInfo = {
+    chainName: "",
+    chainAddress: "",
+    token: null
+  };
+  
+  const delegations = await rest.staking
+    .delegatorDelegations( clients.rest, clients.restAddress )
+    .then( res => res.data.delegation_responses );
+  
+  // console.log(delegations);  
+  for(let item of delegations) {
+
+    let token: ITokenInfo = {
+      denom: convertToken(item.balance.denom),
+      amount: convertAmount(item.balance.amount)
+    };
+    stakeChain = {
+      chainName: "",
+      chainAddress: item.delegation.delegator_address,
+      token: token
+    };
+    MapAccountInfo[accountName].accountStakeInfo.push(stakeChain)
+    // aryStakeChains.push(stakeChain)
+
+  }
+
+  // return stakeChain;
+
 }
 
 function GenerateFee( config : IChainConfiguration ){
@@ -240,6 +427,19 @@ function GenerateFee( config : IChainConfiguration ){
     ],
     gas: "200000", // 200k
   };
+}
+
+function convertToken( denom : string ): string{
+  if (tokenMap[denom]) {
+    return tokenMap[denom]
+  } else {
+    return denom;
+
+  }
+}
+
+function convertAmount( amount : string ): number{
+  return Number(amount) / 1000000;
 }
 
 main();
--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 Stake bot for Cosmos based blockchains (post-Stargate). Give it access to your wallet, point it at a few chains and let it do its thing! Schedule for automatic re-staking, optimizing compounding interest.
 
+Suport multiple wallets, use "accountConfigs.json" to configure mnemonics instead of command line. Please follow the sample config file "accountConfigs_sample.json". The min claimable amount is defined in a global variable called "MIN_REWARD_AMOUNT", which means the amount of token is larger than it will be claimed.
+
 ## Usage
 Built on node, you can run the application in two ways...
 
@@ -13,8 +15,9 @@ The application supports the following chains:
 * Stargaze
 * Like
 * Desmos
+* Secret(To be enhanced, rpc endpoint can not be connected)
 
-It supports recieving your mnemonic token via command line, environment variables and by pulling it directly from Azure Key Vault. If you want to deploy the application somewhere in a hosted environment like a public cloud, make sure you protect your secrets as best you can. Might be a good idea to deploy a separate wallet for the assets you want to restake. It's up to you. 
+If you want to deploy the application somewhere in a hosted environment like a public cloud, make sure you protect your secrets as best you can. Might be a good idea to deploy a separate wallet for the assets you want to restake. It's up to you. 
 
 ### From the command-line
 
@@ -29,29 +32,17 @@ Install dependencies
 
 The available CLI paramaters are as follows:
 
-* **--chainNames**: *(string)* Comma separated list of names for chains you want to process
-  * Corresponding environment variable: `STAKEBOT_CHAIN_NAMES`
-* **--mnemonic**: *(string)* Your wallet mnemonic
-  * Corresponding environment variable: `STAKEBOT_MNEMONIC`
-* **--keyVaultName**: *(string)* Azure Key Vault name 
-  * Corresponding environment variable: `STAKEBOT_KEY_VAULT_NAME`
-  * Please note that when using Azure Key Vault, the application also expects the following environment variables to be defined: `AZURE_CLIENT_SECRET`, `AZURE_CLIENT_ID`, `AZURE_TENANT_ID`
-* **--keyVaultSecretName**: *(string)* Name of secret inside Azure Key Vault
-  * Corresponding environment variable: `STAKEBOT_KEY_VAULT_SECRET_NAME`
-* **--claimRewards**: *(boolean)* Sets whether to claim available rewards
+* **--claimRewards**: *(boolean)* Sets whether to claim available rewards, if it's false, the script will query and store all available balances and staking balances in a global map variable called "MapAccountInfo", which is printed in the console for now.
   * Corresponding environment variable: `STAKEBOT_CLAIM_REWARDS`
 * **--stakeAvailableBalance**: *(boolean)* Sets whether to restake whatever balance is available after rewards have been collected
   * Corresponding environment variable: `STAKEBOT_STAKE_AVAILABLE_BALANCE`
 * **--leaveMinimumBalance**: *(integer)* The minimum balance in a given denom to for leave behind when staking. Also half of the minimum amount to consider it's time to withdraw rewards. NOTE! So far applies to all chains you process at the same time!
   * Corresponding environment variable: `STAKEBOT_LEAVE_MINIMUM_BALANCE`
 
-When using Azure Key Vault, run the application like so:
 
-`ts-node index.ts --chainNames=Cosmos,Chihuahua --keyVaultName=MyKeyVaultName --keyVaultSecretName=name-of-secret  --claimRewards=true --stakeAvailableBalance=true`
+sample command:
 
-When supplying mnemonic directly on the command line, use:
-
-`ts-node index.ts --chainNames=Cosmos,Chihuahua --mnemonic="some days are better than others" --claimRewards=true --stakeAvailableBalance=true`
+`ts-node index.ts --claimRewards=true --stakeAvailableBalance=true`
 
 
 ### In Docker
